f5ffd763412ade1901bbd437dd315e55fd4b8cf7
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-19 23:20:25 -0800

    Output of both my initials
    Output of both my initials.
    Compiled and executed the file to see the result.

--- initials.cpp
+++ initials.cpp
@@ -3,5 +3,18 @@
 
 int main() {
   // The main program code lives here 
-  
+  std::cout<<"RRRRR        S    \n";
+  std::cout<<"R    R     S      \n";
+  std::cout<<"R R       S       \n";
+  std::cout<<"R  R        S     \n";
+  std::cout<<"R   R         S   \n";
+  std::cout<<"R    R      S     \n";
+  std::cout<<"R     R   S       \n";
+
+  return 0;
 }
+
+// Compile and Execute code below in terminal 
+
+// g++ initials.cpp -o initials
+//./initials
