44005d7c338ff98c4105b69c096bbb8965d156f7
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-20 17:24:36 -0800

    Declared and initialized a variable on the same line of code.
    Declared and initialized a variable on the same line of code.

--- variable.cpp
+++ variable.cpp
@@ -3,9 +3,17 @@
 int main() {
   
   // Declare a variable   
-  int year;
+  //int year;
+
+  int score = 0; 
   
   // Initialize a variable
-  year = 2019; 
+  //year = 2019; 
+
+  // Declare and intialize a variable on the same line of code 
+  int year = 2019; 
+
+
+  return 0;
    
 }
