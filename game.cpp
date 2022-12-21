5624bdb88c8384669945816fa1d3f3e41c756580
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-20 17:36:20 -0800

    Chaining of multiple << cout <<
    Chaining of multiple << cout <<

---
+++ game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  
+  int score = 0;
+  
+  // Output
+  std::cout << "Player score: " << score << "\n";
+
+  /* 
+
+  *******Compile and Execute code*******
+
+  g++ game.cpp 
+  ./a.out 
+
+
+  */   
+   
+}
