376f0a865d0d9ebee08bbc324fca88a49bf7bbfc
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-20 17:29:41 -0800

    Performed arithmetic operations.
    Performed arithmetic operations.

---
+++ math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+  int score = 0;
+
+  // Change score here:
+  score = 1234 * 99;
+
+  std::cout << score << "\n";
+
+
+  // Compile and Execute code
+  //     g++ math.cpp
+  //    ./a.out 
+
+}
