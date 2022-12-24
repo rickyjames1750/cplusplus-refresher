ce1144ef31ca43f9b79e47e8aa2c983ba0c96f5e
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-23 22:07:59 -0800

    Corrected Spelling Error
    Corrected Spelling Error

--- variable.cpp
+++ variable.cpp
@@ -10,7 +10,7 @@ int main() {
   // Initialize a variable
   //year = 2019; 
 
-  // Declare and intialize a variable on the same line of code 
+  // Declare and initialize a variable on the same line of code 
   int year = 2019; 
 
 
