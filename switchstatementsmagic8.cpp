5a522bdde31453ecf6c624c9a27952e982e8c67e
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-25 22:46:35 -0800

    Added some switch cases

--- switchstatementsmagic8.cpp
+++ switchstatementsmagic8.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <cstdlib>
 
 int main() {
   //
-  
+  std::cout << "MAGIC 🎱 SAYS: \n\n";
+
+  srand(time(NULL));
+
+  int choice = std::rand() % 10;
+
+  switch(choice) {
+    case 0:
+    std::cout << "No. Not really.\n";
+    break;
+    case 1:
+    std::cout << "Val!\n";
+    break;
+    case 2:
+    std::cout << "You are an adult.\n";
+    break;
+    case 3:
+    std::cout << "Four R's, my friend: Reduce, reuse, recycle, Rihanna.\n";
+    break;
+    case 4: 
+    std::cout << "Nomo FOMO.\n";
+    break;
+    }
 }
