b8101efc06a211e175aa70624814d1ca874ef181
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-26 21:18:01 -0800

    Creating a story via conditionals and for loop
    Creating a story via conditionals and for loop

---
+++ text-adventure.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  
+  std::cout << "It's been a strange day indeed. You've fallen down a rabbit hole, happened across a strange tea party, and had many other absurd adventures.\n";
+  std::cout << "But now, you are called to a trial of some sort, ushered in by a Gryphon.\n";
+}
