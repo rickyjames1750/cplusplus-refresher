bd2270265881650d7304b4a734c2edf8fbae22c3
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-26 17:05:24 -0800

    Declared four variables
    Declared four variables that initializes each at zero

--- sortingthat.cpp
+++ sortingthat.cpp
@@ -2,5 +2,10 @@
  
 int main() {
   // The magic starts here
+  // Declared four variables that initializes each at zero 
+  int gryffindor = 0;
+  int hufflepuff = 0;
+  int ravenclaw = 0;
+  int slytherin = 0; 
  
 }
