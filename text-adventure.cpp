5ac82d3664fa0fd76f5a223e576d9c658744aab4
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-26 21:26:41 -0800

    Added Intro Story and little animation
    Added Intro Story and little animation

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -2,6 +2,19 @@
 
 int main() {
   
+//Intro Story   
   std::cout << "It's been a strange day indeed. You've fallen down a rabbit hole, happened across a strange tea party, and had many other absurd adventures.\n";
   std::cout << "But now, you are called to a trial of some sort, ushered in by a Gryphon.\n";
+  std::cout << "The King and Queen of Hearts are seated on their throne when you arrive, with a great crowd assembled about them — all sorts of little birds and beasts, as well as the whole pack of cards.\n";
+  std::cout << "The White Rabbit is standing near the King, with a trumpet in one hand, and a scroll of parchment in the other.\n";
+
+// Little animation 
+  std::cout << "\n";
+  std::cout << "     \\\\\n";
+  std::cout << "      \\\\_\n";
+  std::cout << "      ( _\\Dn==(\n";
+  std::cout << "      / \\__|\n";
+  std::cout << "     / _/`\"`\n";
+  std::cout << "    {\\  )_\n";
+  std::cout << "     `\"\"\"`\n";
 }
