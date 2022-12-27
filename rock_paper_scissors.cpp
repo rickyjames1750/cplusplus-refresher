8611fde7e03e5a307b19f13c372fb4cbbe9ac207
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-26 18:37:43 -0800

    Wrote randomization code and prompt
    Wrote randomization code and prompt

--- rock_paper_scissors.cpp
+++ rock_paper_scissors.cpp
@@ -3,8 +3,22 @@ This program will allow you to play Rock Paper Scissors against the computer.
 */
 
 #include <iostream>
+#include <stdlib.h>
 
 int main() {
-  // Live long and prosper 
+  // Live long and prosper
+  srand (time(NULL));
+  int computer = rand() % 3 + 1;
+  int user = 0;
+
+  std::cout << "==============\n";
+  std::cout << "rock paper scissors!\n";
+  std::cout << "==============\n";
+
+  std::cout << "1) ✊\n";
+  std::cout << "2) ✋\n";
+  std::cout << "3) ✌️\n";
+
+  std::cout << "shoot! "; 
   
 }
