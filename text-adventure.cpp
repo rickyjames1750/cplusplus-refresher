5f93c75e0797abb5f5ff294afc9bab5ed9ef2c9f
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-26 21:42:05 -0800

    Modified the story and added for loop for choice 1
    Modified the story and added for loop for choice 1

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -3,10 +3,10 @@
 int main() {
   
 //Intro Story   
-  std::cout << "It's been a strange day indeed. You've fallen down a rabbit hole, happened across a strange tea party, and had many other absurd adventures.\n";
-  std::cout << "But now, you are called to a trial of some sort, ushered in by a Gryphon.\n";
-  std::cout << "The King and Queen of Hearts are seated on their throne when you arrive, with a great crowd assembled about them — all sorts of little birds and beasts, as well as the whole pack of cards.\n";
-  std::cout << "The White Rabbit is standing near the King, with a trumpet in one hand, and a scroll of parchment in the other.\n";
+  std::cout << "It's been a strange day indeed. You've fallen down a golden mine hole, happened across a strange taco party, and had many other absurd adventures.\n";
+  std::cout << "But now, you are called to a trial of some sort, ushered in by a Golden Bear.\n";
+  std::cout << "The King (Don Ricardo) and Queen (Calafia) of California are seated on their throne when you arrive, with a great crowd assembled about them — all sorts of little birds and beasts, as well as the whole pack of cards.\n";
+  std::cout << "The Golden Robot is standing near the King, with a trumpet in one hand, and a scroll of parchment in the other.\n";
 
 // Little animation 
   std::cout << "\n";
@@ -18,7 +18,6 @@ int main() {
   std::cout << "    {\\  )_\n";
   std::cout << "     `\"\"\"`\n";
 
-
 // Finishes the intro story 
   std::cout << "\n";
   std::cout << "In the very middle of the court is a table, with a large dish of tarts upon it. They look really good, making you feel quite hungry.\n";
@@ -26,5 +25,18 @@ int main() {
   std::cout << "*      *      *\n";
   std::cout << "What do you do?\n";
 
-  
+
+  char choice1;
+
+  for (int i = 0; i < 3 && choice1 != 'A' && choice1 != 'B' && choice1 != 'C'; i++) {
+
+
+    std::cout << "Enter A if you ask the Golden Bear next to you what the jurors are doing\n";
+    std::cout << "Enter B if you sneak a tart.\n";
+    std::cout << "Enter C if you sit and wait for the trial to begin.\n";
+    std::cin >> choice1;
+    
+  }
+
+
 }
