ddf63f047379067c23e7f983c79f1a4c059b20ba
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-26 23:00:10 -0800

    Added Char Vector
    With added values using vowels.push_back()

--- whale-talk.cpp
+++ whale-talk.cpp
@@ -6,5 +6,18 @@ int main() {
   // Hola, Whale, whale, whale. 
   // What have we got here? 
 
+  // Input string for program to translate 
   std::string input = "Turpentine and Turtles.";
+
+
+  // Created Char vector named vowels
+  std::vector<char> vowels;
+
+  // we can use push_back to add to vector
+  vowels.push_back('a');
+  vowels.push_back('e');
+  vowels.push_back('i');
+  vowels.push_back('o');
+  vowels.push_back('u');
+  // Whales don't consider y as a vowel so we skip it 
 }
