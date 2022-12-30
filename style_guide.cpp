a409c86098c9cba43fdfa62fd958ceee2c1ee68a
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-29 20:23:50 -0800

    Added Include Statements

--- style_guide.cpp
+++ style_guide.cpp
@@ -1,5 +1,5 @@
 /*
-Why care about style? 
+Vamos! Why care about style? 
 
 Style refers to the conventions that govern how we write code. 
 A clean style keeps complex C++ code manageable and readable. 
@@ -12,9 +12,9 @@ look at the following two blocks of C++ code:
 
 using namespace std;
 
-// This program prints out the famous "Hello World!"; 
+// This program prints out the famous "Hello World!" with a twist; 
 int main() {
-    string message = "Hello World!\n";
+    string message = "Hello California!\n";
     cout << message;
     return 0;
 }
@@ -31,3 +31,30 @@ int main() { // Does some stuff
 Style is very much a subjective matter. 
 For the sake of consistency, I will use Google's C++ Style Guide. 
 */
+
+
+
+/*
+Include Statements
+
+#include statements give us access to functionalities from header file libraries. 
+As a rule of thumb, include statememnts are mostly written at the beginning of any C++ program.
+Thus, Include headers should be listed in the following order:
+
+1. C system headers 
+2. C++ standard library headers 
+3. User defined libraries' headers
+*/
+
+// Example below 
+
+// C system headers
+#include <stdlib.h>
+
+// C++ standard library headers 
+#include <iostream>
+
+// User-defined headers 
+#include "foo/server/bar.h"
+
+// The rest of one's code goes here... 
